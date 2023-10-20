ff8fce5061017d9688241dd2ec6d22e7b8d7ee1a
Nitish kumar <krnitish540@gmail.com>
2023-10-20 12:13:47 +0530

    Add Database

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 import express from 'express'
+import mongoose ,{ Schema,model } from 'mongoose';
+import Dotenv from 'dotenv'
+Dotenv.config()
+ 
 
 
 const app=express();
 app.use(express.json())
 const PORT =5000
 
-const student=[]
 
-app.get('/students',(req,res)=>{
+const storedData= async ()=>{
+  const data= await mongoose.connect(process.env.MONGO_URI)
+  if(data){
+    console.log("Connect To MongoDB")
+  }
+}
+storedData()
+
+const studentSchema=new Schema({
+  name:String,
+  age:Number,
+  mobile:String,
+  email:String
+})
+const students=model('students',studentSchema)
+
+app.get('/students',async (req,res)=>{
+  const list= await students.find()
     res.json({
         status:'ok',
-        data:student,
+        data:list,
         massage:'Student featch successfully'
     })
 })
 
 
- app.post('/student',(req,res)=>{
+ app.post('/student',async (req,res)=>{
     const {name,age,mobile,email}=req.body
-      const id=Math.floor(Math.random()*100000)
       if(!name){
         return res.json({
           status:'error',
@@ -37,34 +56,33 @@ app.get('/students',(req,res)=>{
           message:"please provide mobile number"
         })
       }
-      const obj={
-        id:id,
-        name:name,
-        age:age,
-        mobile:mobile,
-        email
-      }
-
-      student.push(obj)
+     const saveStudent= new students({
+      name:name,
+      age:age,
+      mobile:mobile,
+      email:email
+     })
+     const savelist=await saveStudent.save()
 
     res.json({
         status:'ok',
-        list:obj,
+       list:savelist,
         massage:'Data Add successfully'
     })
  })
 
- app.get('/student',(req,res)=>{
+ app.get('/student',async (req,res)=>{
   const {name} = req.query;
-  let result;
-  student.forEach((obj)=>{
-    if(obj.name===name){
-      result=obj;
-    }
-  })
+  const getOne= await students.findOne({name:name}) 
+  if(getOne==null){
+    return res.json({
+      status:'error',
+      message:`${name} not found`
+    })
+  }
   res.json({
     status:'ok',
-    data:result,
+    data:getOne,
     massage:'Data fetched Successfully'
   })
  })
